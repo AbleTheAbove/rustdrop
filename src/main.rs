7a5978a5d9792608a151df8df222c64c4ef7ef21
able <abl3theabove@gmai.com>
2021-02-24 16:34:26 -0600

    I'm sleepy and don't remember what I made, deal with it later fool

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use clap::{App, Arg, SubCommand};
+use std::path::Path;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+
+fn main() {
+    let recieve_command = SubCommand::with_name("recieve")
+        .about("allows the mode to be set to recieve")
+        .version(VERSION)
+        .author(AUTHORS)
+        .arg(
+            Arg::with_name("auto")
+                .short("aa")
+                .help("Automatically accept and save all drops"),
+        );
+
+    let drop_command = SubCommand::with_name("drop")
+        .about("allows the mode to be set to drop")
+        .version(VERSION)
+        .author(AUTHORS)
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .help("Select the file to send.")
+                .takes_value(true)
+                .required(true),
+        );
+
+    let matches = App::new("Rust Drop")
+        .version(VERSION)
+        .author(AUTHORS)
+        .about("An attempt at a rusty AirDrop like LAN file share app!")
+        .arg(
+            Arg::with_name("version")
+                .short("v")
+                .help("Display the version of Rust Drop"),
+        )
+        .subcommand(drop_command)
+        .subcommand(recieve_command)
+        .get_matches();
+    let rdrop = RDrop {};
+    // Print Version info if requested
+    if matches.is_present("v") {
+        println!("Version: {:?}", VERSION);
+    } else {
+    }
+
+    // Switch to match statement
+    if let Some(matches) = matches.subcommand_matches("drop") {
+        if matches.is_present("file") {
+            rdrop.drop(matches.value_of("file").unwrap());
+        } else {
+            println!("Printing normally...");
+        }
+    }
+
+    //don't auto start recieve
+    //rdrop.recieve();
+}
+
+#[derive(Copy, Clone)]
+struct RDrop {}
+impl RDrop {
+    pub fn drop(self, file_path: &str) {
+        if Path::new(&file_path).exists() {
+            //Validate the file exists
+            println!("File found! Dropping {:?}", &file_path);
+        } else {
+            println!("File {:?} not found", &file_path);
+        }
+    }
+    fn recieve(self) {
+        // NOTE(Able): Yeah good luck buddy
+    }
+}
---
+++ src/recieve.rs
@@ -0,0 +1,27 @@
+use std::any::Any;
+use std::sync::Arc;
+use std::time::Duration;
+use zeroconf::prelude::*;
+use zeroconf::{MdnsBrowser, ServiceDiscovery};
+
+pub fn browse() {
+    let mut browser = MdnsBrowser::new("_http._tcp");
+
+    browser.set_service_discovered_callback(Box::new(on_service_discovered));
+
+    let event_loop = browser.browse_services().unwrap();
+
+    loop {
+        // calling `poll()` will keep this browser alive
+        event_loop.poll(Duration::from_secs(0)).unwrap();
+    }
+}
+
+fn on_service_discovered(
+    result: zeroconf::Result<ServiceDiscovery>,
+    _context: Option<Arc<dyn Any>>,
+) {
+    println!("Service discovered: {:?}", result.unwrap());
+
+    // ...
+}
